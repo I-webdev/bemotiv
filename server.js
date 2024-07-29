be0e628b8af5e540a650f1b49998c7e0fa3d8820
i-webdev <idowutemitayoisaiah@gmail.com>
2024-07-29 20:37:10 +0100

    update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static("Public"));
 
 app.get("/", async(req, res)=>{
