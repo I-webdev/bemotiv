e543c943613c78881cb5101d339441ceea37f1ee
Tayoid <idowutemitayoisaiah@gmail.com>
2024-08-13 20:04:13 +0100

    last commit

--- server.js
+++ server.js
@@ -1,40 +1,33 @@
 import express from "express";
-import bodyParser from "body-parser";
+
 import axios from "axios";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("Public"));
 
-app.get("/Bemotiv", async(req, res)=>{
-try{
-    const response = await axios.get("https://quote-api-rust.vercel.app");
-    res.render("index.ejs", {result: response.data});
-}catch(error){
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://quote-api-snowy.vercel.app/random");
+    const motivationObject = response.data;
+    res.render("index.ejs", { result: motivationObject });
+  } catch (error) {
+    console.log(error);
     res.status(500).send("This page can not be rendered.");
-}
+  }
 });
 
-app.get("/:_id", async(req, res)=>{
-       
-    try{
-        const response = await axios.get(
-          "https://quote-api-rust.vercel.app/motivate"
-        );
-        const id = parseInt(req.params._id);
-    console.log(id);
-    const foundJoke = response.data.find((motivation)=>motivation._id === id);
-    
-        res.render("index.ejs", {result: foundJoke});
-    }catch(error){
-        res.status(500).send("This page can not be rendered.");
-    }
-    });
+app.get("/:index", async (req, res) => {
+  try {
+    let endPoint = req.params.index;
+    const response = await axios.get(`https://quote-api-snowy.vercel.app/motivate/${endPoint}`);
 
+    res.render("index.ejs", { result: response.data });
+  } catch (error) {
+    res.status(500).send("This page can not be rendered.");
+  }
+});
 
-
-
-app.listen(port, ()=>{
-    console.log(`This app is running of https://localhost:${port}.`);
-})
+app.listen(port, () => {
+  console.log(`This app is running of https://localhost:${port}.`);
+});
