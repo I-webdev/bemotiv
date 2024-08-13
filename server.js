e3de21da1106a76e0a360d915a9456bfdfb0c81e
Tayoid <idowutemitayoisaiah@gmail.com>
2024-08-13 20:43:10 +0100

    last commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from "express";
-import ejs from "ejs";
+// import ejs from "ejs";
 import axios from "axios";
 const app = express();
 const port = process.env.PORT || 3000;
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 app.use(express.static("Public"));
 
 app.get("/", async (req, res) => {
